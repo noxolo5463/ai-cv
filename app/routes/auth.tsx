b6f4594b2e9233006e3229329114b71801f4989a
noxolo5463 <noxy627@gmail.com>
2025-08-07 19:54:52 +0200

    authorisationWIP

---
+++ app/routes/auth.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export const Auth: () => JSX.Element = () => {
+    return (
+        <div className="auth-container">
+            Authentication is not implemented yet.
+        </div>
+    );
+}
